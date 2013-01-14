fc62dd66b188182fb8e1f1920ac3ff3f2851d0c4
flohannes <flohannes@gmail.com>
2013-01-14 16:21:29 +0000

    getter setter in gaming field

--- MPI_1213_Project/src/mpi1213/isag/view/GamingField.java
+++ MPI_1213_Project/src/mpi1213/isag/view/GamingField.java
@@ -2,8 +2,28 @@ package mpi1213.isag.view;
 
 public class GamingField {
 
+	private boolean isMultiplayer;
+	private boolean isPvP;
+	
 	public GamingField(){
-		
+		setMultiplayer(false);
+		setPvP(false);
+	}
+
+	public boolean isMultiplayer() {
+		return isMultiplayer;
+	}
+
+	public void setMultiplayer(boolean isMultiplayer) {
+		this.isMultiplayer = isMultiplayer;
+	}
+
+	public boolean isPvP() {
+		return isPvP;
+	}
+
+	public void setPvP(boolean isPvP) {
+		this.isPvP = isPvP;
 	}
 	
 }
