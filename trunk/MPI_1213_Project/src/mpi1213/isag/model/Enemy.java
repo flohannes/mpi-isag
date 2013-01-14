2c51fec92435687efd18970cd89bd002f8b19189
flohannes@gmail.com <flohannes@gmail.com@b89e4d25-19dd-9db4-635c-f362dc8e200f>
2013-01-14 15:59:55 +0000

    Enemy

--- trunk/MPI_1213_Project/src/mpi1213/isag/model/Enemy.java
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/Enemy.java
@@ -1,5 +1,33 @@
 package mpi1213.isag.model;
 
 public class Enemy {
+	
+	private int xPos;
+	private int yPos;
+	
+	private int width;
+	private int height;
+	
+	private int deltaX;
+	private int deltaY;
+	
+	public void move(){
+		xPos += deltaX;
+		yPos += deltaY;
+	}
+	
+	public Enemy() {
+		this.width = 50;
+		this.height = 30;
+		this.xPos = 0;
+		this.yPos = 200;
+		this.deltaX = 2;
+		this.deltaY = 0;
+	}
+	
+	public boolean isHit(int xPos, int yPos) {
+		return xPos >= this.xPos && xPos <= (this.xPos + width)
+				&& yPos >= this.yPos && yPos <= (this.yPos + height);
+	}
 
 }
