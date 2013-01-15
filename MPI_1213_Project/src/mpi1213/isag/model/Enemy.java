6bf2d379402fdcc07dcd4a4c2f452c8e5b5f8f9b
dorianok <dorianok@gmail.com>
2013-01-15 21:38:47 +0000

    multiple enemies in different size and speed

--- MPI_1213_Project/src/mpi1213/isag/model/Enemy.java
+++ MPI_1213_Project/src/mpi1213/isag/model/Enemy.java
@@ -2,6 +2,13 @@ package mpi1213.isag.model;
 
 
 public class Enemy{
+	
+	public static final int MIN_WIDTH = 50;
+	public static final int MIN_HEIGHT = 50;
+	public static final int MAX_WIDTH = 250;
+	public static final int MAX_HEIGHT = 200;
+	public static final int MIN_DELTA = 1;
+	public static final int MAX_DELTA = 3;
 		
 	private int xPos;
 	private int yPos;
@@ -11,17 +18,26 @@ public class Enemy{
 	
 	private int deltaX;
 	private int deltaY;
-		
-	public void move(int width, int height){
-		xPos += deltaX;
-		yPos += deltaY;
-		
-		if(xPos < 0 || xPos > width){
-			deltaX = -deltaX;
-		}
-		if(yPos < 0 || yPos > height){
-			deltaY = -deltaY;
-		}
+	
+	//temporary colors
+	private float r = 100;
+	private float g = 100;
+	private float b = 100;
+	
+	public Enemy(double x, double y, double width, double height, double deltaX, double deltaY, float r, float g, float b){
+		this(x, y, width, height, deltaX, deltaY);
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	
+	public Enemy(double x, double y, double width, double height, double deltaX, double deltaY){
+		this.xPos = (int)x;
+		this.yPos = (int)y;
+		this.width = (int)width;
+		this.height = (int)height;
+		this.deltaX = (int)deltaX;
+		this.deltaY = (int)deltaY;
 	}
 	
 	public Enemy() {
@@ -31,6 +47,18 @@ public class Enemy{
 		this.yPos = 200;
 		this.deltaX = 2;
 		this.deltaY = 0;
+	}
+	
+	public void move(int windowWidth, int windowHeight){
+		xPos += deltaX;
+		yPos += deltaY;
+		
+		if(xPos < 0 || xPos > windowWidth){
+			deltaX = -deltaX;
+		}
+		if(yPos < 0 || yPos > windowHeight){
+			deltaY = -deltaY;
+		}
 	}
 	
 	public boolean isHit(int xPos, int yPos) {
@@ -52,5 +80,17 @@ public class Enemy{
 	
 	public float getHeight(){
 		return height;
+	}
+
+	public float getR() {
+		return r;
+	}
+
+	public float getG() {
+		return g;
+	}
+
+	public float getB() {
+		return b;
 	}
 }
