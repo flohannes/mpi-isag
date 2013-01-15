67019a59f9caa5676644ce1668b138f7b2ff35c0
dorianok@gmail.com <dorianok@gmail.com@b89e4d25-19dd-9db4-635c-f362dc8e200f>
2013-01-15 11:13:25 +0000

    first demo with one enemy

--- trunk/MPI_1213_Project/src/mpi1213/isag/model/Player.java
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/Player.java
@@ -3,10 +3,12 @@ package mpi1213.isag.model;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
+import mpi1213.isag.controller.GestureRecognizer;
 import processing.core.PVector;
 
 public class Player implements MouseMotionListener{
 	private PVector targetPosition = new PVector();
+	private GestureRecognizer gestureRecognizer = new GestureRecognizer();
 	
 	public PVector getPosition(){
 		return targetPosition;
@@ -26,5 +28,9 @@ public class Player implements MouseMotionListener{
 	public void mouseMoved(MouseEvent e) {
 		targetPosition.x = (float)e.getX();
 		targetPosition.y = (float)e.getY();
+	}
+
+	public boolean recognizeGesture(PVector hand3d) {
+		return gestureRecognizer.isPushGesture(hand3d);
 	}
 }
---
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/PushListener.java
@@ -0,0 +1,7 @@
+package mpi1213.isag.model;
+
+import processing.core.PVector;
+
+public interface PushListener {
+	public void pushed(PVector vector);
+}
