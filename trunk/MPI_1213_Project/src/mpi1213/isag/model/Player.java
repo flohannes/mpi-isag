cb643f5e8c8fe779643e6823817fbe784a4546a1
flohannes@gmail.com <flohannes@gmail.com@b89e4d25-19dd-9db4-635c-f362dc8e200f>
2013-01-17 12:49:44 +0000

    player methoden hinzugefügt

--- trunk/MPI_1213_Project/src/mpi1213/isag/model/Player.java
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/Player.java
@@ -7,8 +7,13 @@ import mpi1213.isag.controller.GestureRecognizer;
 import processing.core.PVector;
 
 public class Player implements MouseMotionListener{
+	
+	private static int MAX_MUNITION = 10;
+	
 	private PVector targetPosition = new PVector();
 	private GestureRecognizer gestureRecognizer = new GestureRecognizer();
+	private int points;
+	private int munition;
 	
 	public PVector getPosition(){
 		return targetPosition;
@@ -32,5 +37,32 @@ public class Player implements MouseMotionListener{
 
 	public boolean recognizeGesture(PVector hand3d) {
 		return gestureRecognizer.isPushGesture(hand3d);
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public int getMunition() {
+		return munition;
+	}
+
+	public void setMunition(int munition) {
+		this.munition = munition;
+	}
+	
+	public void increasePoints(int pointsToAdd){
+		points += pointsToAdd;
+	}
+	
+	public boolean isMunitionEmpty(){
+		if(munition == 0){
+			return true;
+		}
+		return false;
 	}
 }
