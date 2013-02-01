da7516085586c58d5faa9fe7adee0bc68fca9c49
dorianok@gmail.com <dorianok@gmail.com@b89e4d25-19dd-9db4-635c-f362dc8e200f>
2013-02-01 17:13:47 +0000

    removed background from images
    created first multiplayer menu
    
    kinect image visible if player not calibrated

--- trunk/MPI_1213_Project/src/mpi1213/isag/model/Enemy.java
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/Enemy.java
@@ -1,102 +1,101 @@
 package mpi1213.isag.model;
 
-import java.io.File;
-
-import javax.imageio.ImageIO;
-
 import mpi1213.isag.main.MainApplet;
-
 import processing.core.PImage;
 
+public class Enemy {
 
-public class Enemy{
-	
 	public static final int MIN_WIDTH = 50;
 	public static final int MIN_HEIGHT = 50;
 	public static final int MAX_WIDTH = 250;
 	public static final int MAX_HEIGHT = 200;
 	public static final int MIN_DELTA = 1;
 	public static final int MAX_DELTA = 3;
-		
+
 	private int xPos;
 	private int yPos;
-	
+
 	private int width;
 	private int height;
-	
-	private int deltaX;
-	private int deltaY;
-	
+
+	private double deltaX;
+	private double deltaY;
+
 	private PImage image;
-	
-	//temporary colors
+
+	// temporary colors
 	private float r = 100;
 	private float g = 100;
 	private float b = 100;
+
+	public Enemy(int width, int height) {
+		double deltaX, deltaY;
+		deltaX = (Enemy.MIN_DELTA + Math.random() * (Enemy.MAX_DELTA - Enemy.MIN_DELTA + 1)) * getRandomDirection();
+		deltaY = (Enemy.MIN_DELTA + Math.random() * (Enemy.MAX_DELTA - Enemy.MIN_DELTA + 1)) * getRandomDirection();
+
+		init(Math.random() * width, Math.random() * height, Enemy.MIN_WIDTH + Math.random() * (Enemy.MAX_WIDTH - Enemy.MIN_WIDTH + 1),
+				Enemy.MIN_WIDTH + Math.random() * (Enemy.MAX_WIDTH - Enemy.MIN_WIDTH + 1), deltaX, deltaY, 10 + (float) Math.random() * (255 - 10),
+				(float) Math.random() * (255 - 10), (float) Math.random() * (255 - 10));
+	}
 	
-	public Enemy(double x, double y, double width, double height, double deltaX, double deltaY, float r, float g, float b){
-		this(x, y, width, height, deltaX, deltaY);
+	private double getRandomDirection(){
+		if (Math.random() < 0.5){
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+	private void init(double x, double y, double width, double height, double deltaX, double deltaY, float r, float g, float b) {
+		this.xPos = (int) x;
+		this.yPos = (int) y;
+		this.width = (int) width;
+		this.height = (int) height;
+		this.deltaX = (int) deltaX;
+		this.deltaY = (int) deltaY;
 		this.r = r;
 		this.g = g;
 		this.b = b;
-	}
-	
-	public Enemy(double x, double y, double width, double height, double deltaX, double deltaY){
-		this.xPos = (int)x;
-		this.yPos = (int)y;
-		this.width = (int)width;
-		this.height = (int)height;
-		this.deltaX = (int)deltaX;
-		this.deltaY = (int)deltaY;
 		try {
-			this.image = (PImage)MainApplet.getRandomAlienImage().clone();
+			this.image = (PImage) MainApplet.getRandomAlienImage().clone();
 			image.resize(this.width, 0);
 			this.height = image.height;
 		} catch (CloneNotSupportedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public Enemy(int width, int height){
-		this(Math.random() * width, Math.random() * height, Enemy.MIN_WIDTH + Math.random()
-				* (Enemy.MAX_WIDTH - Enemy.MIN_WIDTH + 1), Enemy.MIN_WIDTH + Math.random() * (Enemy.MAX_WIDTH - Enemy.MIN_WIDTH + 1),
-				Enemy.MIN_DELTA + Math.random() * (Enemy.MAX_DELTA - Enemy.MIN_DELTA + 1), Enemy.MIN_DELTA + Math.random()
-						* (Enemy.MAX_DELTA - Enemy.MIN_DELTA + 1), 10 + (float) Math.random() * (255 - 10), (float) Math.random()
-						* (255 - 10), (float) Math.random() * (255 - 10));
-	}
-	
-	public void move(int windowWidth, int windowHeight){
+
+	public void move(int windowWidth, int windowHeight) {
 		xPos += deltaX;
 		yPos += deltaY;
-		
-		if(xPos < 0 || xPos > windowWidth){
+
+		if (xPos < 0 || xPos > windowWidth) {
 			deltaX = -deltaX;
 		}
-		if(yPos < 0 || yPos > windowHeight){
+		if (yPos < 0 || yPos > windowHeight) {
 			deltaY = -deltaY;
 		}
 	}
-	
+
 	public boolean isHit(int xPos, int yPos) {
-		return xPos >= this.xPos && xPos <= (this.xPos + width)
-				&& yPos >= this.yPos && yPos <= (this.yPos + height);
+		return xPos >= this.xPos && xPos <= (this.xPos + width) && yPos >= this.yPos && yPos <= (this.yPos + height);
 	}
-	
-	public float getX(){
+
+	public float getX() {
 		return xPos;
 	}
-	
-	public float getY(){
+
+	public float getY() {
 		return yPos;
 	}
-	
-	public float getWidth(){
+
+	public float getWidth() {
 		return width;
 	}
-	
-	public float getHeight(){
+
+	public float getHeight() {
 		return height;
 	}
 
@@ -111,8 +110,8 @@ public class Enemy{
 	public float getB() {
 		return b;
 	}
-	
-	public PImage getImage(){
+
+	public PImage getImage() {
 		return this.image;
 	}
 }
---
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/PlayerButton.java
@@ -0,0 +1,12 @@
+package mpi1213.isag.model;
+
+import processing.core.PImage;
+
+public class PlayerButton extends Button {
+
+	public PlayerButton(int x, int y, int width, int height, String text, PImage image) {
+		super(x, y, width, height, text, image);
+		// TODO Auto-generated constructor stub
+	}
+
+}
