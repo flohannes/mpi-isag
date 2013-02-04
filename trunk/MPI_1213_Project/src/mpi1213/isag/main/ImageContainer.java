f49c11e86110f11cfa96d8f5bca22e12b904051e
dorianok@gmail.com <dorianok@gmail.com@b89e4d25-19dd-9db4-635c-f362dc8e200f>
2013-02-04 16:48:05 +0000

    more bonus items

--- trunk/MPI_1213_Project/src/mpi1213/isag/main/ImageContainer.java
+++ trunk/MPI_1213_Project/src/mpi1213/isag/main/ImageContainer.java
@@ -18,6 +18,7 @@ public class ImageContainer {
 	public static PImage coop;
 	public static PImage pvp;
 	public static PImage freezeItem;
+	public static PImage unlimitedAmmo;
 	
 	public static void initImages(PApplet applet) {
 		backgroundImage = applet.loadImage("Images/sternenhimmel.jpg");
@@ -31,6 +32,7 @@ public class ImageContainer {
 		coop = applet.loadImage("Images/coop.png");
 		pvp = applet.loadImage("Images/pvp.png");
 		freezeItem = applet.loadImage("Images/freeze_item.png");
+		unlimitedAmmo = applet.loadImage("Images/unlimited_ammo.png");
 		alien1 = applet.loadImage("Images/alien_01.png");
 		alien2 = applet.loadImage("Images/alien_02.png");
 		alien3 = applet.loadImage("Images/alien_03.png");
--- trunk/MPI_1213_Project/src/mpi1213/isag/model/bonus/FreezeItem.java
+++ trunk/MPI_1213_Project/src/mpi1213/isag/model/bonus/FreezeItem.java
@@ -2,23 +2,11 @@ package mpi1213.isag.model.bonus;
 
 import mpi1213.isag.main.ImageContainer;
 
-public class FreezeItem extends BonusItem{
+public class FreezeItem extends BonusItem {
 
-	public static long FREEZE_TIME = 2000;
-	
-	private long freezeTime = FREEZE_TIME;
-	
 	public FreezeItem(int windowWidth, int windowHeight) {
 		super(windowWidth, windowHeight);
 		imageInit(ImageContainer.freezeItem, windowWidth, windowHeight);
-	}
-
-	public long getFreezeTime() {
-		return freezeTime;
-	}
-	
-	public void setFreezeTime(long time){
-		freezeTime = time;
 	}
 
 }
